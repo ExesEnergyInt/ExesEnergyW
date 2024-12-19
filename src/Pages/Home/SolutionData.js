e3bc41ba2bc5443740fc8a0de19e3fdf8bc5059d
Derek Emmanuel <derekemmanuel99@gmail.com>
2024-12-19 12:39:33 +0100

    new commit

--- src/Pages/Home/SolutionData.js
+++ src/Pages/Home/SolutionData.js
@@ -2,17 +2,20 @@ export const SoluData = [
   {
     image: "/Images/forHero/Rectangle21.png",
     heading: "Commercial",
-    text: "Offers guidance and support to the executive team while contributing to strategic planning. As a counselor, he provides valuable insight and assistance to members on various personal and professional matters.",
+    text: "As solar technology continues to improve and costs continue to decrease, more businesses will make the switch to solar energy. The future is bright for solar power, and those who adopt it now will be well-positioned to succeed in a green, sustainable economy.",
+    link: "/Articles/Commart",
   },
 
   {
     image: "/Images/forHero/Rectangle30.png",
     heading: "Industrial",
-    text: "Offers guidance and support to the executive team while contributing to strategic planning. As a counselor, he provides valuable insight and assistance to members on various personal and professional matters.",
+    text: "Industries that haven’t yet explored solar energy should start considering it as a viable option for both cost savings and sustainability. Now is the time for industries to take action and invest in solar energy for a brighter, greener future.",
+    link: "/Articles/Indusart",
   },
   {
     image: "/Images/forHero/Rectangle16.png",
     heading: "Residential",
-    text: "Offers guidance and support to the executive team while contributing to strategic planning. As a counselor, he provides valuable insight and assistance to members on various personal and professional matters.",
+    text: "Solar energy allows remote workers to be more flexible and mobile. Portable solar systems can be used in different locations, whether at home, in a remote cabin, or while traveling, making it easier to work from almost anywhere.",
+    link: "/Articles/Resdart",
   },
 ];
